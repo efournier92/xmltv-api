5811c309f1055c825ee2a3fe214e0e2860e3f82b
efournier92 <efournier92@gmail.com>
2017-10-24 19:44:11 -0400

    initial commit

--- app_api/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var xmltvCtrl = require('../controllers/xmltv.controller');
-
-// XMLTV API 
-router.get('/xmltv', xmltvCtrl.getXml);
-
-module.exports = router;
-
