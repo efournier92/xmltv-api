3f3efc95452cdb4a3024cda9dd5c362d2b667087
efournier92 <efournier92@gmail.com>
2017-10-28 19:25:36 -0400

    refactor index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,31 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var jwt = require('express-jwt');
-
-var auth = jwt({
-  secret: process.env.JWT_SECRET,
-  userProperty: 'payload'
-});
-
-var authCtrl = require('../controllers/auth.controller');
-var userCtrl = require('../controllers/user.controller');
 var commonCtrl = require('../controllers/common.controller');
 
 // Auth API 
-router.post('/register', authCtrl.register);
-router.post('/login', authCtrl.login);
-
-// Ico API
-router.get('/ico', commonCtrl.getIcons);
-
-// Ico API
-router.get('/defaultgrid', commonCtrl.getDefaultGrid);
-
-// User API
-router.get('/user', auth, userCtrl.profileRead);
-
-// User Widget API
-router.put('/user', auth, userCtrl.updateWidgets);
+router.post('/xmltv', xmltvCtrl.fetchXml);
 
 module.exports = router;
 
