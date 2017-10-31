0cc55cf6c10ede1822760ccb5b229a3d8af77e6c
efournier92 <efournier92@gmail.com>
2017-10-31 18:06:50 -0400

    translate cache cleanup logic

--- app_api/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var xmltvCtrl = require('../controllers/xmltv.controller');
-
-// Auth API 
-router.get('/xmltv', xmltvCtrl.getXml);
-
-module.exports = router;
-
