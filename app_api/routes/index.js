51d95effc235746368b628379424e04d649dd9d3
efournier92 <efournier92@gmail.com>
2017-11-07 18:00:46 -0500

    finish project cleanup

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var xmltvCtrl = require('../controllers/xmltv.controller');
 
-// Auth API 
+// XMLTV API 
 router.get('/xmltv', xmltvCtrl.getXml);
 
 module.exports = router;
