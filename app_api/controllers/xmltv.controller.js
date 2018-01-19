bae8a6831cfcc7273b5bc452935f7462bbbdd2c5
efournier92 <efournier92@gmail.com>
2018-01-19 14:39:24 -0500

    make html fetcher work!

--- app_api/controllers/xmltv.controller.js
+++ app_api/controllers/xmltv.controller.js
@@ -1,8 +1,25 @@
-zapData = require(`../get_data/getZap2It.service`);
+const zapData = require(`../get_data/getZap2It.service`);
+const http = require(`http`);
+const fs = require('fs');
 
 module.exports.getXml = function (req, res) {
-  zapData.getZap();
-
-    res.status(200).json(`{message:SUCCESS!}`);
-
+  let today = new Date;
+  let ms = today.getTime();
+  const url = 
+    `http://tvlistings.zap2it.com/tvlistings/ZCGrid.do?aid=zap2it.html`;
+  // `http://tvlistings.zap2it.com/tvlistings/ZCGrid.do?isDescriptionOn=true&amp;fromTimeInMillis=${ms}&amp;aid=zap2it`;
+  let body = ``;
+  http.get(url, res => {
+    res.on(`data`, function (chunk) {
+      body += chunk;
+    });
+    res.on(`end`, function () {
+      fs.writeFile(`test.html`, body, function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log(`FILE SAVED!`);
+      }); 
+    });
+  });
 };
